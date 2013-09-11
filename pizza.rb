6e5020052a5df4355694be520aa61cfacf29b3de
Daniel Gamble McAdam <daniel.gamble.mcadam@gmail.com>
2013-09-11 14:42:05 -0500

    add initialize method with toppings attribute for pie class

--- pizza.rb
+++ pizza.rb
@@ -1,9 +1,29 @@
 module Pizza
   class Pie
+    attr_accessor :toppings
+    # Instantiate a new pie.
+    #
+    # topping - The String name of the topping.
+    #
+    # Returns a new Pie object.
+    def initialize(toppings)
+       @toppings = toppings
+
+    end
 
   end
 
   class Topping
-
+    attr_accessor :name, :vegetarian
+    # Instantiate a new topping.
+    #
+    # name - The String name of the topping.
+    # vegetarian - Boolean value indicating topping is vegetarian
+    #
+    # Returns a new Topping object.
+    def initialize(name, vegetarian: false)
+      @name = name
+      @vegetarian = vegetarian
+    end
   end
 end
